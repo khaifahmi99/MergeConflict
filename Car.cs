4943878b31132f0d963f8d60bdf0091e5bcbc3bb
khaifahmi99 <48536235+khaifahmi99@users.noreply.github.com>
2019-03-16 00:51:29 +1100

    Create class car with constructor

---
+++ Car.cs
@@ -0,0 +1,23 @@
+public class Car {
+  private string _model;
+  private string _color;
+  private int _yearReleased;
+  private float _price;
+  
+  public string Model {
+    get{ return _model; }
+    set { _model = value; }
+  }
+  
+  public Car(string model, string color, int yearReleased, float price) {
+    _model = model;
+    _color = color;
+    _yearReleased = yearReleased;
+    _price = price;
+  }
+  
+  public void Drive(){
+    
+  }
+  
+}
