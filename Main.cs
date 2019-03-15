e4fad914c7ea7cb3efc9f63d8714e8245de7689b
khaifahmi99 <48536235+khaifahmi99@users.noreply.github.com>
2019-03-16 01:00:09 +1100

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,9 @@
+class Program {
+  static void Main(string[] args){
+    Car car1 = new Car("Ferrari", "Red",1999, 500000.00);
+    Car car2 = new Car("Porsche", "Black", 2006, 350000.00);
+    
+    car1.drive();
+    car2.drive();
+  }
+}
